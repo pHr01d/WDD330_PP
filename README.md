1aec14f556a4f42b7d87954ceaaba5c94e300fca
pHr01d <pro20006@byui.edu>
2024-07-04 18:22:09 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# fmdb
+
+## Description
+
+Family Movie Database is a web app that manages a locally stored database of movies that your family enjoys.
+
+## Features
+
+-- Retrieve information from a global movie database with all the details of a favorite movie
+-- Store movie information locally
+-- Manage local data to family preferences
+-- User login manages access for admins to change database content and limits which movies can be listed based on genre, rating, etc.
+
---
+++ src/js/utils.mjs
@@ -0,0 +1,69 @@
+// wrapper for querySelector...returns matching element
+export const qs = (selector, parent = document) => parent.querySelector(selector);
+
+// retrieve data from localstorage
+export function getLocalStorage(key) { return JSON.parse(localStorage.getItem(key));}
+
+// save data to local storage
+export function setLocalStorage(key, data) { localStorage.setItem(key, JSON.stringify(data));}
+
+// set a listener for both touchend and click
+export function setClick(selector, callback) {
+  qs(selector).addEventListener("touchend", (event) => {
+    event.preventDefault();
+    callback();
+  });
+  qs(selector).addEventListener("click", callback);
+}
+
+// Returns parameters from URL
+export function getParam(param) {
+  const
+    queryString = window.location.search,
+    urlParams = new URLSearchParams(queryString),
+    product = urlParams.get(param);
+  return product;
+}
+
+export function renderListWithTemplate(templateFn, parentElement, list, position = "afterbegin", clear = true) {
+  if (clear) {
+    parentElement.innerHTML = " ";
+  }
+  const htmlStrings = list.map(templateFn); //map method breaking out the array
+  parentElement.insertAdjacentHTML(position, htmlStrings.join(""));
+}
+
+export async function renderWithTemplate(templateFn, parentElement, data, callback, position = "afterbegin", clear = true) {
+  if (clear) {
+    parentElement.innerHTML = " ";
+  }
+  const htmlString = await templateFn(data);
+  parentElement.insertAdjacentHTML(position, htmlString);
+  if (callback) {
+    callback(data);
+  }
+}
+
+function loadTemplate(path) {
+  // wait what?  we are returning a new function? 
+  // this is called currying and can be very helpful.
+  return async function () {
+    const res = await fetch(path);
+    if (res.ok) {
+      const html = await res.text();
+      return html;
+    }
+  };
+} 
+
+export async function loadHeaderFooter() {
+  const
+    headerTemplateFn = loadTemplate("../assets/parts/header.html"),
+    headerEl = qs("#main-header");
+  renderWithTemplate(headerTemplateFn, headerEl);
+
+  const
+    footerTemplateFn = loadTemplate("../assets/parts/footer.html"),
+    footerEl = qs("#main-footer");
+  renderWithTemplate(footerTemplateFn, footerEl);
+}
